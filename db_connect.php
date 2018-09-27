d8d6298f55bc910c9ad9ff79fda62209d9411a35
erwin-iconi <43653164+erwin-iconi@users.noreply.github.com>
2018-09-27 21:56:32 +0200

    Database aanmaken bestand
    Code om DB aan te maken

---
+++ db_connect.php
@@ -0,0 +1,10 @@
+<?php
+    $servername = "localhost";
+    $username = "deb25567_30orless";
+    $password = "Jq8xWRVLi";
+    $dbname = "deb25567_30orless";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
